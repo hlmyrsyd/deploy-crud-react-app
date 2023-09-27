88aefe69fb24c752a1d708034330c86efd26b91e
jodyboy <elite.crew66@gmail.com>
2023-09-27 20:49:46 +0700

    Change Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css';
 import { SignIn, SignUp, MainMenu } from './containers';
-import { HashRouter as BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 
 function App() {
